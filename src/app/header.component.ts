c6c904d0bf2fe45ed4667b81bea0519d8cc31ed9
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-04-17 15:15:22 +0200

    meho

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
     selector: 'app-header',
     // template: '<h1>Hello World</h1>' - DO NOT USE AN INLINE ELEMENT
     standalone: true,
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 
 export class HeaderComponent {}
