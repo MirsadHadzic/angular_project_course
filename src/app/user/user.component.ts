b1f637a051af9bc31bd978d431d8b4bb9d7d4de3
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-04-21 14:01:25 +0200

    komitan kod za slektanje nekih idjeva

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 // import { Component, computed, signal } from '@angular/core';
-import { Component, Input, computed, input} from '@angular/core';
+import { Component, EventEmitter, Input, Output, computed, input} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { computeMsgId } from '@angular/compiler';
 
@@ -12,43 +12,28 @@ import { computeMsgId } from '@angular/compiler';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string;
+  @Input({required: true}) id!: string
+  @Input({required: true}) avatar!: string;
   // // definitely will be a string value 
-  // @Input({required: true}) name!: string;
+  @Input({required: true}) name!: string;
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
   // imagePath = computed(() => './assets/users/' + this.selectedUser().avatar);
-
-  // get imagePath()
-  // {
-  //   return  'assets/users/' + this.selectedUser.avatar;
-  // }
+  @Output() select = new EventEmitter();
 
 
-  // onSelectedUser()
-  // {
-  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  //   // this.selectedUser = DUMMY_USERS[randomIndex];
-  //   console.log('Clicked');
-  // }
-
-  // signal components that are down over here cannot be changed because they are read-only signals
-  // avatar = input<string>();
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
-
-  // get imagePath()
-  // {
+  // imagePath = computed(() => {
   //   return 'assets/users/' + this.avatar;
-  // }
+  // });
+
+  get imagePath()
+  {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() 
   {
     // this.avatar.set();
+    this.select.emit(this.id);
   }
 
 }
