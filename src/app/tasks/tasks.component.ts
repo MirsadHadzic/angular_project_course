d5caf27ff567ab5c222459719349cfd230a7aa17
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-04-22 15:57:18 +0200

    neke formalne jos stvari vezano za angular

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,44 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { identifierName } from '@angular/compiler';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: String | undefined;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name?: string;
+  tasks = 
+  [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: "Learn all the basic and advanced features of Angular & how to apply them.", 
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master Angularr',
+      summary: "Learn all the basic and advanced features of Angular & how to apply them.", 
+      dueDate: '2025-10-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Master Angularrr',
+      summary: "Learn all the basic and advanced features of Angular & how to apply them.", 
+      dueDate: '2025-11-31'
+    }
+  ];
+
+  get SelectedUserTasks()
+  {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
