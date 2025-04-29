58ea27277f66ac468ba1cb73ac1477f451760e62
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-04-29 18:05:10 +0200

    committred some more modules stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+// import { HeaderComponent } from './header/header.component';
+// import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
+// import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  standalone: false,
+  // imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
